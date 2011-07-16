1d43163d7d1bfe7c378afc13537068673eb240c0
Brian Bauer <brian.bauer@g5platform.com>
2011-07-16 13:58:51 -0700

    Fix error in emails model

--- app/mailers/emails.rb
+++ app/mailers/emails.rb
@@ -1,9 +1,9 @@
 class Emails < ActionMailer::Base
-  def contactme(emails_params, sent_at = Time.now)
+  def contactme(emails_params)
     subject "Electronic Mail: " << emails_params[:subject]
-    recipients "brianebaoptipng file1.pnguer@gmail.com"
+    recipients "brianebauer@gmail.com"
     from emails_params[:address]
-    sent_on sent_at
+    sent_on Time.now
     body :message => emails_params[:body], :sender_name => emails_params[:name]
   end
 end
