b73305602ddc2bbe38f5415c9e56ab3f06e660d7
Brian Bauer <brian.bauer@g5platform.com>
2011-10-20 09:12:11 -0700

    Add html5 shiv

--- app/admin/case_studies.rb
+++ app/admin/case_studies.rb
@@ -1,9 +1,10 @@
 ActiveAdmin.register CaseStudy do
-  form do |f|
-    f.inputs "Details" do
-      f.input :name
-      f.input :content, :as => :ckeditor
-    end
-    f.buttons
-  end
+  # form do |f|
+  #   f.inputs "Details" do
+  #     f.input :name
+  #     f.input :image01
+  #     f.input :content, :as => :ckeditor
+  #   end
+  #   f.buttons
+  # end
 end
